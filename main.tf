c14eed39e8476b90ddaf72f7cd203f560b7e3949
Cristian Ramirez <cristiano.rosetti@gmail.com>
2023-11-13 11:46:42 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,30 +9,30 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.77.0"
+#module "vpc" {
+#  source  = "terraform-aws-modules/vpc/aws"
+#  version = "2.77.0"
 
-  name                 = "education"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
+#  name                 = "education"
+#  cidr                 = "10.0.0.0/16"
+#  azs                  = data.aws_availability_zones.available.names
+#  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+#  enable_dns_hostnames = true
+#  enable_dns_support   = true
+#}
 
 resource "aws_db_subnet_group" "education" {
-  name       = "education"
-  subnet_ids = module.vpc.public_subnets
+  name       = var.db_name
+  subnet_ids = var.public_subnets
 
   tags = {
-    Name = "Education"
+    Name = "PoV-Demo"
   }
 }
 
 resource "aws_security_group" "rds" {
-  name   = "education_rds"
-  vpc_id = module.vpc.vpc_id
+  name   = var.db_name
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 5432
@@ -49,12 +49,12 @@ resource "aws_security_group" "rds" {
   }
 
   tags = {
-    Name = "education_rds"
+    Name = "pov_demo_rds"
   }
 }
 
 resource "aws_db_parameter_group" "education" {
-  name   = "education"
+  name   = var.db_name
   family = "postgres14"
 
   parameter {
@@ -64,7 +64,7 @@ resource "aws_db_parameter_group" "education" {
 }
 
 resource "aws_db_instance" "education" {
-  identifier             = "education"
+  identifier             = var.db_name
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
