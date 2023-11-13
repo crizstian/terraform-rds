8bd20a3ef484a6582ad46e9483838347a72f7ea7
Cristian Ramirez <cristiano.rosetti@gmail.com>
2023-11-13 11:44:30 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,9 @@
 # SPDX-License-Identifier: MPL-2.0
 
 provider "aws" {
-  region = var.region
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 data "aws_availability_zones" "available" {}
