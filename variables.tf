ddf2d1c9cac2fa38b24581e1745f43923681fa42
Cristian Ramirez <cristiano.rosetti@gmail.com>
2023-11-17 11:21:10 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,9 @@ variable "access_key" {}
 variable "secret_key" {}
 variable "db_name" {}
 variable "vpc_id" {}
-variable "public_subnets" {}
+variable "public_subnets" {
+  type = list(string)
+}
 
 variable "db_password" {
   description = "RDS root user password"
